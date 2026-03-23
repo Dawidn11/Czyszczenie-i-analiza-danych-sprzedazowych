0ce6886c3c7531d1d4740d294238e1b63cfb7c5b
Dawidn11 <daawid099@wp.pl>
2026-03-23 10:55:44 +0100

    Create czyszczenie_danych.py

---
+++ czyszczenie_danych.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("brudne_dane.csv",decimal=",")
+
+pd.set_option('display.max_columns', None)
+print(df.head(10))
+
+df = df.dropna(how="all")
+df = df.replace(["ERROR", "UNKNOWN"], np.nan)
+
+df["Transaction ID"] = df["Transaction ID"].str.replace("TXN_","")
+df["Transaction ID"] = pd.to_numeric(df["Transaction ID"], errors = "coerce")
+df["Total Spent"] = pd.to_numeric(df["Total Spent"], errors="coerce")
+df["Quantity"] = pd.to_numeric(df["Quantity"], errors="coerce")
+df["Price Per Unit"] = pd.to_numeric(df["Price Per Unit"], errors="coerce")
+df["Transaction Date"] = pd.to_datetime(df["Transaction Date"], errors="coerce").dt.date
+
+df["Total Spent"] = df["Total Spent"].fillna(df["Quantity"] * df["Price Per Unit"])
+df["Quantity"] = df["Quantity"].fillna(df["Total Spent"]/df["Price Per Unit"])
+df["Price Per Unit"] = df["Price Per Unit"].fillna(df["Total Spent"] / df["Quantity"])
+
+
+df = df.dropna(subset=["Total Spent"])
+df = df.dropna(subset=["Item", "Quantity"], how="all")
+
+print(df.isnull().sum())
+print(df.info())
+
+
+
+df.to_excel("clean_cafe_sales.xlsx", index = False)
